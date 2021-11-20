cacecef791255a6b1ef81e9374cf1e904c11996c
mizonovakristina <miss.cristel@mail.ru>
2021-11-20 22:28:54 +0300

    Все файлы

--- desctop_project/wpack1/src/index.js
+++ desctop_project/wpack1/src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './benksy.jpg';
 
 function component(){
 	const element = document.createElement('div');
 	element.innerHTML = _.join(['Webpack','тест'],' ');
 	console.log("Создан div элемент")
+	element.classList.add('test');
+	console.log("Применён стиль класса .test")
+	const myIcon = new Image();
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
 	return element;
 }
 document.body.appendChild(component());
--- desctop_project/wpack1/webpack.config.js
+++ desctop_project/wpack1/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
+	devServer: {static: './dist',},
+	devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
@@ -10,6 +13,17 @@ module.exports = {
 	module: {
 		rules: [
 		{ test: /\.css$/, use: ['style-loader', 'css-loader']},
+		{test:/\.(png|svg|jpg|gif)$/, use: ['file-loader',]},
+			{test: /\.m?js$/, exclude: /(node_modules|bower_components)/,
+			use: {loader: 'babel-loader',
+			options: {presets: ['@babel/preset-env'],
+			plugins: ['@babel/plugin-proposal-object-rest-spread']
+			}
+			}
+			},
 		]
 	},
+	plugins: [new HtmlWebpackPlugin({
+		title: 'Webpack test plug',
+	})],
 };
